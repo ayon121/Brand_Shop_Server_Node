04c35b842419b0c001a1f0f2262f4a9d934df370
ayon121 <borshonsaha1234@gmail.com>
2023-10-19 16:08:05 +0600

    add product done

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = "mongodb+srv://borshonsaha1234:TLqQE4ymjG3zYyQT@cluster0.6rjuyq3.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+
+    await client.connect();
+    const database = client.db("ProductsDB");
+    const usercollections = database.collection("Products");
+
+    app.get('/productDetails' , async(req , res) => {
+        const cursor= usercollections.find()
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+    app.get('/poducts/:id' , async(req , res)=> {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const user = await usercollections.findOne(query);
+      res.send(user)
+    })
+    app.post('/productDetails' , async(req , res) => {
+        const user = req.body;
+        console.log('new' , user);
+        const result = await usercollections.insertOne(user);
+        res.send(result)
+    })
+    app.delete('/products/:id' , async(req , res) => {
+      const  id = req.params.id
+      console.log('plz delete  from database' , id);
+      const query = {_id : new ObjectId(id)}
+      const result = await usercollections.deleteOne(query)
+      res.send(result)
+    })
+    app.put('/products/:id' , async(req , res)=> {
+      const id = req.params.id;
+      const User = req.body;
+      console.log(id ,User);
+      const filter = {_id : new ObjectId(id)}
+      const options = {upsert : true}
+      const  updateUser = { 
+        $set : {
+          name: User.name,
+          email : User.email
+        }
+      }
+      const result = await usercollections.updateOne(filter , updateUser , options)
+      res.send(result)
+
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Brand Shop management server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+})
+
+// user
+// borshonsaha1234
+// password
+// TLqQE4ymjG3zYyQT
