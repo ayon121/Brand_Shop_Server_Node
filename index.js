45f28fe587d176f8dcf1f6ecdd7d761c5820c964
ayon121 <borshonsaha1234@gmail.com>
2023-10-20 20:28:15 +0600

    full server deploy done

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-
+require('dotenv').config()
 // middleware
 app.use(cors())
 app.use(express.json())
 
 
 
-const uri = "mongodb+srv://borshonsaha1234:TLqQE4ymjG3zYyQT@cluster0.6rjuyq3.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6rjuyq3.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
